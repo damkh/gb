3cbe7363610eb76db6584f3b5e49bdfbe73b931c
u4 <foo@bar.baz>
2020-02-26 05:09:31 +0300

    L1: rename dz_X to dz_1_X

--- gb2/L1/Khamadiyarov_Damir_DZ_1/dz_1_2.py
+++ gb2/L1/Khamadiyarov_Damir_DZ_1/dz_1_2.py
@@ -1,4 +1,5 @@
+seconds = 86400
+print('342342 should be 95:5:42')
+print('86400 should be 24:0:0')
 #seconds = int(input('Enter time in seconds: '))
-seconds = 342342
-print('342342 should be 95:05:42')
 print(f'{seconds} seconds is: {seconds // 3600}:{(seconds - (seconds // 3600) * 3600) // 60}:{seconds % 60}')
