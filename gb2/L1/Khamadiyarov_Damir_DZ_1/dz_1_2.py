c2a64075d356750b618de4a8ed62631de6cc69f7
u4 <foo@bar.baz>
2020-02-26 05:18:33 +0300

    DZ_1 deleted

--- gb2/L1/Khamadiyarov_Damir_DZ_1/dz_1_2.py
+++
@@ -1,5 +0,0 @@
-seconds = 86400
-print('342342 should be 95:5:42')
-print('86400 should be 24:0:0')
-#seconds = int(input('Enter time in seconds: '))
-print(f'{seconds} seconds is: {seconds // 3600}:{(seconds - (seconds // 3600) * 3600) // 60}:{seconds % 60}')
--- gb2/L1/Khamadiyarov_Damir_DZ_1/dz_1_3.py
+++
@@ -1,7 +0,0 @@
-num = 3
-# Input
-#num = int(input('Enter your number: '))
-# All from 1 to 9
-#for num in range(1, 10):
-#    print(f'{num} -> n + nn + nnn: {num} + {10*num+num} + {100*num+10*num+num} = {100*num+20*num+3*num}')
-print(f'{num} -> n + nn + nnn: {num} + {10*num+num} + {100*num+10*num+num} = {100*num+20*num+3*num}')
